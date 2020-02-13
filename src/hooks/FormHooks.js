f76d25b89fd499fe2c504ef6d852577196b8f9f7
Lucas Melo <luucasmell@gmail.com>
2020-02-12 21:14:22 -0300

    Create hooks to validate and create telephone and text mask field

--- src/hooks/FormHooks.js
+++ src/hooks/FormHooks.js
@@ -12,3 +12,38 @@ export const useEmailField = initialValue => {
   };
   return [email, handleChange, valid];
 };
+
+export const usePhoneInput = initialValue => {
+  const phoneValidationRegex = /^\(\d{2}\) \d{4,5}-\d{4}/;
+  const [phone, setPhone] = useState(initialValue);
+  const [valid, setValid] = useState(phoneValidationRegex.test(initialValue));
+
+  const handleChange = value => {
+    value = value.replace(/\D/g, "");
+
+    const mask =
+      value.length > 6 ? "($1) $2-$3" : value.length > 2 ? "($1) $2" : "$1";
+    const groupRegex =
+      value.length > 10
+        ? /^(\d{1,2})(\d{0,5})(\d{0,4}).*/
+        : /^(\d{1,2})(\d{0,4})(\d{0,4}).*/;
+
+    value = value.replace(groupRegex, mask);
+    setPhone(value);
+    setValid(phoneValidationRegex.test(value));
+  };
+  return [phone, handleChange, valid];
+};
+
+export const useTextField = initialValue => {
+  const [text, setText] = useState(initialValue);
+  const [valid, setValid] = useState(
+    initialValue !== "" && typeof initialValue !== "number"
+  );
+
+  const handleChange = newValue => {
+    setText(newValue);
+    setValid(newValue !== "" && typeof initialValue !== "number");
+  };
+  return [text, handleChange, valid];
+};
